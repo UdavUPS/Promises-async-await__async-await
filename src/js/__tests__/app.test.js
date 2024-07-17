a931adebfd2c8d046450f55f47deaa82d27e08e9
Udav-94 <ups-fti@yandex.ru>
2024-07-17 18:04:39 +0300

    Переписана функция на асинхронную

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,11 +1,11 @@
-import GameSavingLoader from '../app';
+import load from '../app';
 
-jest.setTimeout(10000);
-test('GameSavingLoader test', async (done) => {
+/* jest.setTimeout(10000); */
+test('load test', async (done) => {
     let result;
-    let testObject = {"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}};
+    let testObject = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
 
-  GameSavingLoader.load().then((saving) => {
+  load().then((saving) => {
     result = saving;
     console.log(result);
   })
--- src/js/app.js
+++ src/js/app.js
@@ -2,38 +2,19 @@
 import json from './parser';
 import read from './reader';
 
-/* export default class GameSavingLoader {
-    static load() {
-      const data = read(); // возвращается Promise!
-      const value = json(data); // возвращается Promise!
-      return value;
-    }
-  } */
-
-/*   function GameSavingLoader() {
-    return new Promise (resolve => {
-        const data = read(); // возвращается Promise!
-        const value = json(data); // возвращается Promise!
-        return value;
-    })
-  } */
-
-    export default class GameSavingLoader {
-        static load() {
-            return new Promise ((resolve, reject) => {
-                setTimeout( () => {
-                    read().then(data => {
-                      json(data).then(e => resolve(e))
-                    })
-                  }, 0);
-            });
-
-        }
-      }
+export default async function load() {
+  try {
+    let data = await read();
+    let value = await json(data);
+    return value;
+  }
+  catch(e) {
+    console.log(e);
+  }
+}
 
 
-  GameSavingLoader.load().then((saving) => {
-    return saving;
-  }, (error) => {
-    console.log(error);
-  });
+load().then((saving) => {
+  result = saving;
+  console.log(result);
+})
